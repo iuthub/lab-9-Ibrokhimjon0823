c49bb514ed7da9bdfd36e3602d7e838c82b3afb3
Ibrokhimjon23 <45231891+Ibrokhimjon23@users.noreply.github.com>
2020-04-13 17:15:08 +0500

    finished all assignments

--- routes/web.php
+++ routes/web.php
@@ -5,49 +5,12 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', [
-    'uses' => 'PostController@getIndex',
-    'as' => 'blog.index'
-]);
-
-Route::get('post/{id}', [
-    'uses' => 'PostController@getPost',
-    'as' => 'blog.post'
-]);
-
-Route::get('about', function () {
-    return view('other.about');
-})->name('other.about');
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('', [
-        'uses' => 'PostController@getAdminIndex',
-        'as' => 'admin.index'
-    ]);
-
-    Route::get('create', [
-        'uses' => 'PostController@getAdminCreate',
-        'as' => 'admin.create'
-    ]);
-
-    Route::post('create', [
-        'uses' => 'PostController@postAdminCreate',
-        'as' => 'admin.create'
-    ]);
-
-    Route::get('edit/{id}', [
-        'uses' => 'PostController@getAdminEdit',
-        'as' => 'admin.edit'
-    ]);
-
-    Route::post('edit', [
-        'uses' => 'PostController@postAdminUpdate',
-        'as' => 'admin.update'
-    ]);
+Route::get('/', function () {
+    return view('welcome');
 });
